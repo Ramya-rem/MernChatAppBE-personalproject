f2600863972b9672c84e6f91fa0d03b2cad6b680
Ramya-rem <ramyasubramaniam7301@gmail.com>
2024-12-11 10:41:31 +0530

    command the frontend index.html in server.js

--- server.js
+++ server.js
@@ -25,11 +25,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist"))) //it is the middleware(static) use to serve static file like html,css
+// app.use(express.static(path.join(__dirname, "/frontend/dist"))) //it is the middleware(static) use to serve static file like html,css
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+
 
 
 // app.get('/',(req,res)=>{
