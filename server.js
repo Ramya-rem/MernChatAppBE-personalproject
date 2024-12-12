fa92fa14cf020b62dc2c389fa5713fd1d2d4d6e3
Ramya-rem <ramyasubramaniam7301@gmail.com>
2024-12-12 09:52:58 +0530

    updating server.js

--- server.js
+++ server.js
@@ -25,35 +25,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist"))) //it is the middleware(static) use to serve static file like html,css
+app.use(express.static(path.join(__dirname, "frontend", "dist"))); //it is the middleware(static) use to serve static file like html,css
 
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-// });
-// Serve static files from the 'frontend/dist' directory
-app.use(express.static(path.join(__dirname, 'frontend', 'dist')));
-
-// Catch-all route to serve the index.html for any other route
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
-
-
-// app.get('/',(req,res)=>{
-//     //root route http://localhost:5000/
-//     res.send("Hello world");
-
-// });
-// app.get("/api/auth/signup",(req,res)=>{
-//     console.log("signup route");
-// });
-// app.get("/api/auth/login",(req,res)=>{
-//     console.log("login route");
-// });
-// app.get("/api/auth/logout",(req,res)=>{
-//     console.log("logout route");
-// });
-//endha mari use panna code neat ah irukadhu so adhukaga namma middleware pandrom routes ku(app.use)
 
 
 server.listen(PORT,()=>{
